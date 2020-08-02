06af2f54ca238aa1808e6b7556fc9dbd6a5ad998
Jose R F Junior <web2ajax@gmail.com>
2020-08-02 14:26:31 -0400

    upgrade service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: true,
   /*PACIENTE   φ = '1,61803398874988'; */
   pessoaURFlux10: 'http://191.252.204.57:8080/pessoa/flux20',
+  funcionarioURFlux10: 'http://191.252.204.57:8080/pessoa/flux20Funcionario',
+  pacienteURFlux10: 'http://191.252.204.57:8080/pessoa/flux20Paciente',
   pacienteURLInsert: 'http://191.252.204.57:8080/pessoa/paciente/post',
 
   /*EXAME   φ = '1,61803398874988'; */
